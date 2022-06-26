796cd430550daec6256c2726bc3a68aff5d13d3a
Isaac Ibiapina <isaac1@mail.usf.edu>
2022-06-25 23:52:49 -0400

    checkpoint: working with stubbed api call

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,108 @@
-import * as S from './styles'
+import * as React from 'react';
+import styled from 'styled-components'
+import {Wrapper, GridItem, Text, TextInput } from '../styles'
+import { Comment } from '../Comment/Comment'
+import { CommentDetails } from '../CommentDetails/CommentDetails'
+import { CommentConfirmation } from '../CommentConfirmation/CommentConfirmation'
+import { GenericMessage } from '../GenericMessage/GenericMessage'
 
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo>Boilerplate</S.Logo>
-    <S.Title>Next.Js Boilerplate</S.Title>
-    <S.Description>
-      Next.Js, React.Js, Typescript e Styled Components
-    </S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Desenvolvedor sentado a frente de um computador"
-    />
-  </S.Wrapper>
-)
+
+import { Message } from '../../types/data'
+import { colors } from '../../constants'
+import { useState } from 'react'
+import { useCallback } from 'react'
+
+const Divider = GridItem
+const MessagesContainer = styled(GridItem)`
+  overflow-x: scroll;
+`
+
+const messages: Array<Message> = [
+  {from: '0x29a73a7a2c3242af9f78dadffb361b361e044b6c',
+  to:'somebody',
+    signature: 'dfsfddsfsafd',
+    time: Date.now(),
+    cid: '32432',
+    text: 'Hey Ethereum bros, what is the up'
+  },
+  {from: '0x29a73a7a2c3242af9f78dadffb361b361e044b6c',
+    to:'somebody',
+    signature: 'dfsfdewsafd',
+    time: Date.now() - 86400000,
+    cid: '32432',
+    text: 'Hey Ethereum bros, what is the up'
+  },
+  {from: '0x29a73a7a2c3242af9f78dadffb361b361e044b6c',
+    to:'somebody',
+    signature: 'dfsfdesafd',
+    time: Date.now(),
+    cid: '32432',
+    text: 'Hey Ethereum bros, what is the up'
+  },
+  {from: '0x29a73a7a2c3242af9f78dadffb361b361e044b6c',
+    to:'somebody',
+    signature: 'dfs3645fdsafd',
+    time: Date.now(),
+    cid: '32432',
+    text: 'Hey Ethereum bros, what is the up'
+  },
+  {from: '0x29a73a7a2c3242af9f78dadffb361b361e044b6c',
+    to:'somebody',
+    signature: 'dfsf3dsafd',
+    time: Date.now(),
+    cid: '32432',
+    text: 'Hey Ethereum bros, what is the up'
+  },  {from: '0x29a73a7a2c3242af9f78dadffb361b361e044b6c',
+    to:'somebody',
+    signature: 'dfsftydsafd',
+    time: Date.now(),
+    cid: '32432',
+    text: 'Hey Ethereum bros, what is the up'
+  }
+]
+
+const submitMessageAPICall = () => Promise.resolve() //TODO update this
+
+//TODO if I have time, add a filter to the top where we can filter from
+const Main = () => {
+  const [shownMessage, setShowDetails] = useState<Message | null>(null)
+  const showDetails = useCallback((m) => setShowDetails(m), [setShowDetails])
+  const hideDetails = useCallback(() => setShowDetails(null), [setShowDetails])
+  const [shownConfirmation, setShowConfirmation] = useState(false)
+  const hideConfirmation = useCallback(() => setShowConfirmation(false), [setShowConfirmation])
+  const [inputValue, setInputValue] = useState('');
+  const showConfirmation = useCallback((e) => e.preventDefault() || (inputValue.length && setShowConfirmation(true)), [setShowDetails, inputValue])
+  const [feedback, setFeedBack] = useState('')
+
+  const onMessageSign = useCallback((message: Message) => {
+    submitMessageAPICall(message).then(success => {
+      setFeedBack('Message was broadcast!')
+      setInputValue('')
+    }).catch(e => setFeedBack('Message was not broadcast!'))
+  }, [setFeedBack, setInputValue])
+
+  return (
+  <Wrapper>
+    <GridItem gridColumn={'2/3'} gridRow={'2/3'}>
+      <Text size={'20px'} color={colors.text.regular}>
+        UniChatScan
+      </Text>
+      </GridItem>
+    <Divider gridColumn={'1/4'} gridRow={'3/4'} style={{borderTop: `1px solid ${colors.border.regular}`}}/>
+    <MessagesContainer gridColumn={'2/3'} gridRow={'4/5'} border={`1px solid ${colors.border.regular}`}>
+      {messages.map(m => (
+        <Comment key={m.signature} message={m} showDetails={() => showDetails(m)}/>
+      ))}
+    </MessagesContainer>
+    {shownMessage && <CommentDetails closeCb={hideDetails} message={shownMessage}/>}
+    {shownConfirmation && <CommentConfirmation closeCb={hideConfirmation} text={inputValue} onMessageSign={onMessageSign}/>}
+    {feedback && <GenericMessage text={feedback} closeCb={() => setFeedBack('')} />}
+    <GridItem gridRow={'5/6'} gridColumn={'2'}>
+      <form onSubmit={showConfirmation}>
+        <TextInput value={inputValue} placeholder={'Broadcast a message'} onChange={e => setInputValue(e.target.value)}/>
+      </form>
+    </GridItem>
+  </Wrapper>
+)}
 
 export default Main
---
+++ src/helpers/helpers.ts
@@ -0,0 +1,17 @@
+export const getTimeText = (time: number): string => {
+  const dateDay = new Date(time).setHours(0, 0, 0, 0);
+  const today = new Date().setHours(0, 0, 0, 0);
+
+  const isToday = today === dateDay;
+  const isYesterday = (today - 86400000) < time
+
+  const date = new Date(time);
+  if (isToday || isYesterday) {
+    const foreText = isToday ? 'Today at' : 'Yesterday at'
+    const options: Intl.DateTimeFormatOptions = { hour: 'numeric', minute: 'numeric' };
+    return `${foreText} ${date.toLocaleDateString('en-US', options).split(',')[1]}`
+  } else {
+    const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  }
+};
